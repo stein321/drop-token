b82947bc63c969eb87e8bab092507f94474eca97
Benjamin Stein <ben@liatrio.com>
2019-05-24 17:43:37 -0700

    initial commit

---
+++ src/main/java/b/stein/droptoken/DropTokenApplication.java
@@ -0,0 +1,13 @@
+package b.stein.droptoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/b/stein/droptoken/DropTokenApplicationTests.java
@@ -0,0 +1,16 @@
+package b.stein.droptoken;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DropTokenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
