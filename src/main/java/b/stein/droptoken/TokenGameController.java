d6280a85107e9b122de5bf5523bba9e13c626ba8
Benjamin Stein <ben@liatrio.com>
2019-05-26 17:53:02 -0700

    add request mappings for create new game and get all games

--- src/main/java/b/stein/droptoken/TokenGameController.java
+++ src/main/java/b/stein/droptoken/TokenGameController.java
@@ -1,14 +1,36 @@
 package b.stein.droptoken;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class TokenGameController {
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello World!";
+    @Autowired
+    private GameRepository repository;
+
+    @RequestMapping(value = "/drop_token", method = RequestMethod.GET)
+    public Map getGames() {
+        HashMap<String, List<String>> map = new HashMap<>();
+        List<String> gameIds = repository.findAll().stream().map(Game::getId).collect(Collectors.toList());
+        map.put("games", gameIds);
+        return map;
+    }
+
+    @RequestMapping(value = "/drop_token", method = RequestMethod.POST)
+    public Map<String, String> createGame(@RequestBody Game game) {
+        Game newGame = repository.insert(game);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("gameId", newGame.getId());
+        return map;
     }
 
 }
