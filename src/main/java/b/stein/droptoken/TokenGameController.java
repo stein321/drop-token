babf0af1af6f18f5a548d5e967ec6931a56298e5
Benjamin Stein <ben@liatrio.com>
2019-05-26 18:17:17 -0700

    method for getting game by ID. may need to do more checking for a malformed request. finished https://trello.com/c/j51F3FfZ/4-get-droptoken-gameid

--- src/main/java/b/stein/droptoken/TokenGameController.java
+++ src/main/java/b/stein/droptoken/TokenGameController.java
@@ -1,10 +1,10 @@
 package b.stein.droptoken;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,7 +17,7 @@ public class TokenGameController {
     @Autowired
     private GameRepository repository;
 
-    @RequestMapping(value = "/drop_token", method = RequestMethod.GET)
+    @RequestMapping(value = "/drop_token/", method = RequestMethod.GET)
     public Map getGames() {
         HashMap<String, List<String>> map = new HashMap<>();
         List<String> gameIds = repository.findAll().stream().map(Game::getId).collect(Collectors.toList());
@@ -25,8 +25,28 @@ public class TokenGameController {
         return map;
     }
 
+    @RequestMapping(value = "/drop_token/{gameId}", method = RequestMethod.GET)
+    public Game getOneGame(@PathVariable("gameId") String id) {
+        Game game;
+        try {
+            game = repository.findGameById(id);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Malformed request"
+            );
+        }
+
+        if (game == null) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Game/moves not found"
+            );
+        }
+        return game;
+    }
+
     @RequestMapping(value = "/drop_token", method = RequestMethod.POST)
     public Map<String, String> createGame(@RequestBody Game game) {
+        //TODO: validate data
         Game newGame = repository.insert(game);
         HashMap<String, String> map = new HashMap<>();
         map.put("gameId", newGame.getId());
