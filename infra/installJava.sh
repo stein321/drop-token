d7f58ab36eadc657be723a100b138ca1c2342c2e
Benjamin Stein <ben@liatrio.com>
2019-07-27 19:23:18 -0700

    remove installJava.sh since it's done inline

--- infra/installJava.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-wget https://d3pxv6yz143wms.cloudfront.net/11.0.3.7.1/java-11-amazon-corretto-devel-11.0.3.7-1.x86_64.rpm
-sudo yum install -y java-11-amazon-corretto-devel-11.0.3.7-1.x86_64.rpm
