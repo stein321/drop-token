d82ca43f6ca252ae758c45ca44ad70851cf83292
Benjamin Stein <ben@liatrio.com>
2019-08-15 19:03:13 -0400

    comment out password updating. need to figure out a better process here for user creation and granting db access to the api

--- infra/installAwsCli.sh
+++ infra/installAwsCli.sh
@@ -2,5 +2,5 @@
 sudo apt update
 sudo apt install -y python3-pip
 sudo pip3 install awscli
-grep 'The default' /home/bitnami/bitnami_credentials | cut -d \' -f4 > password.txt
+# todo: create user and store password in ssm
 #aws ssm put-parameter --name "prod-mongo-password" --type SecureString --overwrite --region us-west-2  --value $(grep 'The default' /home/bitnami/bitnami_credentials | cut -d \' -f4)
